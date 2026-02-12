d28b52f59396fecd00c7134312aab2fcabc0e9de
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-02-12 15:10:03 +0700

    complete query string

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,8 +1,9 @@
 import Search from '@/app/product/search';
 
 export default async function ProductPage(props: PageProps<'/product'>) {
-  const searchParams = await props.searchParams;
-  console.log(searchParams); // { search: 'fgfgfgfg' }
+  const { page, limit, category, search } = await props.searchParams;
+  // getProduct({page, limit. category, search})
+  // console.log(searchParams); // { search: 'fgfgfgfg' }
   // queyy something from database with searchparams
   return (
     <div className="flex flex-col gap-12 p-12">
--- src/app/product/search.tsx
+++ src/app/product/search.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useRef } from 'react';
 
 export default function Search() {
   const pathname = usePathname();
@@ -8,22 +9,31 @@ export default function Search() {
   const searchParams = useSearchParams();
   console.log(searchParams);
   const router = useRouter();
+
+  const inputRef = useRef<HTMLInputElement>(null); // { current: null }
+
   return (
     <>
       <input
         type="text"
         placeholder="Type something ..."
         className="px-3 py-1.5 w-full border outline-none"
+        ref={inputRef} // { current: <input /> }
       />
       <button
         className="border bg-cyan-400 p-3"
         onClick={() => {
-          const params = new URLSearchParams({ a: '10', b: '20' }); // ?a=10&b=20
-          params.append('c', 'hello'); // ?a=10&b=20&c=hello
-          params.append('a', '5'); // ?a=10&b=20&c=hello&a=5
-          params.set('b', '50'); // ?a=10&b=50&c=hello&a=5
-          params.delete('a'); // ?b=50&c=hello
-          console.log(params.toString());
+          // const params = new URLSearchParams({ a: '10', b: '20' }); // ?a=10&b=20
+          // params.append('c', 'hello'); // ?a=10&b=20&c=hello
+          // params.append('a', '5'); // ?a=10&b=20&c=hello&a=5
+          // params.set('b', '50'); // ?a=10&b=50&c=hello&a=5
+          // params.delete('a'); // ?b=50&c=hello
+          // console.log(params.toString());
+
+          const params = new URLSearchParams(searchParams);
+          if (inputRef.current?.value)
+            params.set('search', inputRef.current?.value);
+
           router.push(`${pathname}?${params.toString()}`);
         }}
       >
